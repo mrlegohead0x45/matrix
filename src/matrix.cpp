d4f218b624f276fc76d93d7888adbc5190692a83
mrlegohead0x45 <mrlegohead0x45@gmail.com>
2022-03-10 17:48:56 +0000

    Add Matrix getters

--- src/matrix.cpp
+++ src/matrix.cpp
@@ -1,8 +1,22 @@
 #include "matrix.hpp"
 
+#include <vector>
+
+using std::vector;
+
 template <typename ItemType>
 Matrix<ItemType>::Matrix(const Shape shape) : m_shape(shape) {
     // allocate enough space for a matrix of this shape
     this->m_columns.reserve(this->getShape().getColumns());
     this->m_rows.reserve(this->getShape().getRows());
 };
+
+template <typename ItemType>
+vector<ItemType> Matrix<ItemType>::getColumns() const {
+    return this->m_columns;
+}
+
+template <typename ItemType>
+vector<ItemType> Matrix<ItemType>::getRows() const {
+    return this->m_rows;
+}
