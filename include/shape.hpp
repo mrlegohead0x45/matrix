77fc80861c618edbdf10af855dec7f835d3bf715
mrlegohead0x45 <mrlegohead0x45@gmail.com>
2022-03-10 17:20:22 +0000

    Add Shape ctors and getters
    Minor refactor of CMakeLists.txt
    Add include sorting options to .clang-format

--- include/shape.hpp
+++ include/shape.hpp
@@ -7,11 +7,14 @@ using std::size_t;
 
 class Shape {
   private:
-    const size_t rows, columns;
+    const size_t m_rows, m_columns;
 
   public:
     size_t getRows() const;
     size_t getColumns() const;
+
+    Shape() = default;
+    Shape(size_t rows, size_t columns);
 };
 
 #endif // SHAPE_HPP_
--- src/shape.cpp
+++ src/shape.cpp
@@ -1,1 +1,11 @@
 #include "shape.hpp"
+
+#include <cstddef>
+
+using std::size_t;
+
+Shape::Shape(size_t rows, size_t columns) : m_rows(rows), m_columns(columns){};
+
+size_t Shape::getColumns() const { return this->m_columns; }
+
+size_t Shape::getRows() const { return this->m_rows; }
