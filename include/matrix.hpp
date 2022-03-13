cf56f523ecc0d426718e87233b9662a066cc732f
mrlegohead0x45 <mrlegohead0x45@gmail.com>
2022-03-13 19:03:32 +0000

    Add more Matrix constructors

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -3,25 +3,30 @@
 
 #include "shape.hpp"
 
+#include <cstddef>
 #include <initializer_list>
 #include <vector>
 
-using std::vector, std::initializer_list;
+using std::vector, std::initializer_list, std::size_t;
 
 template <typename ItemType> class Matrix {
   private:
     vector<ItemType> m_columns, m_rows;
-    const Shape m_shape;
+    Shape m_shape;
 
   public:
     vector<ItemType> getColumns() const;
     vector<ItemType> getRows() const;
-    const Shape getShape() const;
+    Shape getShape() const;
 
-    Matrix() = default;
-    Matrix(const Shape);
-    Matrix(initializer_list<Matrix>);
-    Matrix(vector<vector<ItemType>>);
+    Matrix() = default;              // Matrix m
+    Matrix(Shape);             // Matrix m(Shape(2, 2))
+    Matrix(size_t, size_t);// Matrix m(2, 2)
+    Matrix(initializer_list<Shape>); // Matrix m{Shape(2, 2)}
+    // Matrix(initializer_list<Matrix>); // Matrix m{Matrix()}
+    Matrix(vector<vector<ItemType>>); // Matrix m({{1, 2, 3}, {4, 5, 6}})
+    Matrix(
+        initializer_list<vector<ItemType>>); // Matrix m{{1, 2, 3}, {4, 5, 6}}
 };
 
 #endif // MATRIX_HPP_
--- src/matrix.cpp
+++ src/matrix.cpp
@@ -2,16 +2,23 @@
 
 #include "shape.hpp"
 
+#include <cstddef>
 #include <vector>
 
-using std::vector;
+using std::vector, std::size_t;
 
 template <typename ItemType>
-Matrix<ItemType>::Matrix(const Shape shape) : m_shape(shape) {
+Matrix<ItemType>::Matrix(Shape shape) : m_shape(shape) {
     // allocate enough space for a matrix of this shape
     this->m_columns.reserve(this->getShape().getColumns());
     this->m_rows.reserve(this->getShape().getRows());
 };
+
+template <typename ItemType>
+Matrix<ItemType>::Matrix(size_t rows, size_t columns) : m_shape(rows, columns) {
+    this->m_columns.reserve(this->getShape().getColumns());
+    this->m_rows.reserve(this->getShape().getRows());
+}
 
 template <typename ItemType>
 vector<ItemType> Matrix<ItemType>::getColumns() const {
@@ -23,6 +30,6 @@ vector<ItemType> Matrix<ItemType>::getRows() const {
     return this->m_rows;
 }
 
-template <typename ItemType> const Shape Matrix<ItemType>::getShape() const {
+template <typename ItemType> Shape Matrix<ItemType>::getShape() const {
     return this->m_shape;
 }
