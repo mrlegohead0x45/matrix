37f1b673d9d7599fa02641d07fb2b1f7af711eaa
mrlegohead0x45 <mrlegohead0x45@gmail.com>
2022-03-24 19:25:38 +0000

    Implement Matrix::getColumns

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -11,17 +11,17 @@ using std::vector, std::initializer_list, std::size_t;
 
 template <typename ItemType> class Matrix {
   private:
-    vector<ItemType> m_columns, m_rows;
+    vector<vector<ItemType>> m_rows;
     Shape m_shape;
 
   public:
-    vector<ItemType> getColumns() const;
-    vector<ItemType> getRows() const;
+    Matrix<ItemType> getColumns() const;
+    vector<vector<ItemType>> getRows() const;
     Shape getShape() const;
 
     Matrix() = default;              // Matrix m
-    Matrix(Shape);             // Matrix m(Shape(2, 2))
-    Matrix(size_t, size_t);// Matrix m(2, 2)
+    Matrix(Shape);                   // Matrix m(Shape(2, 2))
+    Matrix(size_t, size_t);          // Matrix m(2, 2)
     Matrix(initializer_list<Shape>); // Matrix m{Shape(2, 2)}
     // Matrix(initializer_list<Matrix>); // Matrix m{Matrix()}
     Matrix(vector<vector<ItemType>>); // Matrix m({{1, 2, 3}, {4, 5, 6}})
--- src/matrix.cpp
+++ src/matrix.cpp
@@ -21,12 +21,24 @@ Matrix<ItemType>::Matrix(size_t rows, size_t columns) : m_shape(rows, columns) {
 }
 
 template <typename ItemType>
-vector<ItemType> Matrix<ItemType>::getColumns() const {
-    return this->m_columns;
+Matrix<ItemType> Matrix<ItemType>::getColumns() const {
+    // this should return the first column top-down as a vector, then the second etc
+
+    Matrix columns(this->getRows());
+    // left to right
+    for (size_t col_idx = 0; col_idx < this->getShape().getColumns();
+         col_idx++) {
+        // top to bottom
+        for (size_t row_idx = 0; row_idx < this->getShape().getRows();
+             row_idx++) {
+            columns[col_idx][row_idx] = this->getRows()[row_idx][col_idx];
+        }
+    }
+    return columns;
 }
 
 template <typename ItemType>
-vector<ItemType> Matrix<ItemType>::getRows() const {
+vector<vector<ItemType>> Matrix<ItemType>::getRows() const {
     return this->m_rows;
 }
 
