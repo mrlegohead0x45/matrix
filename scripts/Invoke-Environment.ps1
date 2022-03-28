baa1e89466d3b17dc706e51771b92480581d01df
mrlegohead0x45 <mrlegohead0x45@gmail.com>
2022-03-28 17:43:44 +0100

    Try combining windows and linux

--- scripts/Invoke-Environment.ps1
+++ scripts/Invoke-Environment.ps1
@@ -1,4 +1,5 @@
 # from https://github.com/majkinetor/posh/blob/master/MM_Admin/Invoke-Environment.ps1
+# used to add visual studio environment variables to the github actions environment
 function Invoke-Environment {
     param
     (
@@ -11,6 +12,8 @@ function Invoke-Environment {
     cmd /c "$Command > nul 2>&1 && set" | . { process {
         if ($_ -match '^([^=]+)=(.*)') {
             [System.Environment]::SetEnvironmentVariable($matches[1], $matches[2])
+            # add it to the github actions environment
+            Write-Output "$matches[1]=$matches[2]" >> $env:GITHUB_ENV
         }
     }}
 
