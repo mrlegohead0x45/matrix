27a6197692648fa030a4331447cb99167dc9ac9a
mrlegohead0x45 <mrlegohead0x45@gmail.com>
2022-03-28 18:22:28 +0100

    Add workflows (#1)
    Add cmake workflow to build and test
    Add clang-format workflow to check if pull requests are formatted correctly

---
+++ scripts/Invoke-Environment.ps1
@@ -0,0 +1,20 @@
+# from https://github.com/majkinetor/posh/blob/master/MM_Admin/Invoke-Environment.ps1
+# used to add visual studio environment variables to the github actions environment
+function Invoke-Environment {
+    param
+    (
+        # Any cmd shell command, normally a configuration batch file.
+        [Parameter(Mandatory=$true)]
+        [string] $Command
+    )
+
+    $Command = "`"" + $Command + "`""
+    cmd /c "$Command > nul 2>&1 && set" | . { process {
+        if ($_ -match '^([^=]+)=(.*)') {
+            [System.Environment]::SetEnvironmentVariable($matches[1], $matches[2])
+            # add it to the github actions environment
+            Write-Output ($matches[1] + "=" + $matches[2]) >> $env:GITHUB_ENV
+        }
+    }}
+
+}
